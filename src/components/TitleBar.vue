7b7519a629ae09d159a8d50f15bcc52edb2ba939
zhuozhenwei <zhuozhenwei1024@outlook.com>
2024-06-20 12:54:47 +0800

    加了一句话

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -3,6 +3,7 @@
     <router-link to="/">
       <img src="../assets/logo.png">
     </router-link>
+    <div class="additional-text">(Related paper is under review, only a small part of data published)</div>
   </nav>
 </template>
   
@@ -13,16 +14,34 @@
   </script>
   
   <style scoped>
-  
+/*   
   .title-bar {
     background-color: #d6e6fe;
     color: white;
     height: 100px;
     width:100%;
-  }
+  } */
+  .title-bar {
+  position: relative;
+  background-color: #d6e6fe;
+  color: white;
+  height: 100px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
   img{
     width: 600px;
     height: 100px;
   }
+  .additional-text {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 5px;
+  background-color: #d6e6fe;
+  color: rgb(50, 50, 50);
+}
   </style>
   
